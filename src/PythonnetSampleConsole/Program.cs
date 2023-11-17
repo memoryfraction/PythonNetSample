6ce8f43747211a69456767e8fb283a53c51b4dfa
Rong(Rex) Fan || 樊荣 <rex.fan18@gmail.com>
2023-11-17 14:27:43 -0500

    VectorBT的测试用例已经跑通

--- src/PythonnetSampleConsole/Program.cs
+++ src/PythonnetSampleConsole/Program.cs
@@ -10,11 +10,11 @@ namespace PythonnetSampleConsoleApp
         static void Main(string[] args)
         {
             // 配置并准备PythonEngine
-            var pythonHomePath = @"D:\ProgramData\anaconda3\envs\python39";//创建一个python39的虚拟环境，并指向其路径;
-            if(!Directory.Exists(pythonHomePath))
+            var pythonHomePath = @"C:\Users\Rex\.conda\envs\myenvpy39";//创建一个python39的虚拟环境，并指向其路径;
+            if (!Directory.Exists(pythonHomePath))
                 throw new DirectoryNotFoundException(pythonHomePath);
             var dllFilePath = $@"{pythonHomePath}\python39.dll"; // 请确保指向正确的 Python DLL 路径, pythonnet兼容python39
-            if(!File.Exists(dllFilePath))
+            if (!File.Exists(dllFilePath))
                 throw new FileNotFoundException(dllFilePath);
 
             // 对应Python内的重要路径
@@ -34,27 +34,10 @@ namespace PythonnetSampleConsoleApp
             PythonEngine.PythonHome = pythonHomePath;
             PythonEngine.PythonPath = pySearchPath;
 
-            PythonEngine.Initialize();
-            using (Py.GIL())
-            {
-                dynamic np = Py.Import("numpy");
-                Console.WriteLine(np.cos(np.pi * 2));
+            VectorBT.VectorBTMethod();
 
-                dynamic sin = np.sin;
-                Console.WriteLine(sin(5));
+            Console.ReadKey();
 
-                double c = (double)(np.cos(5) + sin(5));
-                Console.WriteLine(c);
-
-                dynamic a = np.array(new List<float> { 1, 2, 3 });
-                Console.WriteLine(a.dtype);
-
-                dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
-                Console.WriteLine(b.dtype);
-
-                Console.WriteLine(a * b);
-                Console.ReadKey();
-            }
         }
     }
 }
