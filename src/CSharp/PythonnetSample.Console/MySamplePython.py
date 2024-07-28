9420767ac84a4c84e66876cab70ff2b7b5a29025
Rong(Rex) Fan || 樊荣 <rong.fan2020@outlook.com>
2024-07-27 21:57:34 -0400

    实现了两个方法 1 C# Python混合编程 2 调用Pyhton脚本，需要依赖于环境中的包

---
+++ src/CSharp/PythonnetSample.Console/MySamplePython.py
@@ -0,0 +1,3 @@
+
+def say_hello():
+    return "hello world! I am a python script!"
---
+++ src/CSharp/PythonnetSample.Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using Python.Runtime;
+
+
+
+namespace PythonnetSampleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 初始化变量
+            var condaVenvHomePath =  @"D:\ProgramData\PythonVirtualEnvs\pair_trading";
+            var infra = PythonNetInfra.GetPythonInfra(condaVenvHomePath,  "python39.dll");
+            Runtime.PythonDLL = infra.PythonDLL;
+            PythonEngine.PythonHome = infra.PythonHome;
+            PythonEngine.PythonPath = infra.PythonPath;
+            
+            PythonEngine.Initialize();// 初始化Python引擎
+            // 使用Python GIL
+            using (Py.GIL())
+            {
+                // sample1 获取数据，并打印; 期望：打印获取的金融数据;
+                dynamic data = YfinanceSample();
+                // 打印股票数据
+                Console.WriteLine(data);
+
+                // sample2， 期望：打印：hello world
+                RunScript("MySamplePython", "say_hello");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scriptFileName">scriptFileName 需要copy Always</param>
+        /// <param name="methodName"></param>
+        static void RunScript(string scriptFileName, string methodName)
+        {
+            var pythonScript = Py.Import(scriptFileName);
+            var response = pythonScript.InvokeMethod(methodName);
+            Console.WriteLine(response);
+        }
+
+
+        /// <summary>
+        /// 获取数据，并返回
+        /// </summary>
+        /// <returns></returns>
+        static dynamic YfinanceSample()
+        {
+            // 导入yfinance并获取股票数据
+            dynamic yf = Py.Import("yfinance");
+            dynamic ticker = yf.Ticker("AAPL");
+            dynamic data = ticker.history(period: "1y");
+            return data;
+        }
+    }
+}
